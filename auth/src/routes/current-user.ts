e5ad91ac73ef3d9b65b23b1a6d8892234b557d01
victor rosa <vicrosa25@gmail.com>
2021-04-24 11:27:18 +0200

    currentUser end point added

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hey there");
+  // 1. Check if jwt exist
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  // 2. Check if jwt is valid
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };

7125b4257af2b46241661e18cff271c0eaee139b
victor rosa <vicrosa25@gmail.com>
2021-07-23 11:44:17 +0200

    Added serverSideProps function working

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,20 @@
-export default function home() {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
+};
+
+export async function getServerSideProps({ req }) {
+  const { data } = await axios.get(
+    "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+    {
+      headers: req.headers,
+    }
+  );
+  return {
+    props: data,
+  };
 }
+
+export default LandingPage;

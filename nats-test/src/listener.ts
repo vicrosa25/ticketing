97f41bcb294aab5899e4d986108a937e58ed2f89
victor rosa <vicrosa25@gmail.com>
2021-08-09 10:38:01 +0200

    create Nats code in common module

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/tikect-create-listener";
 
 console.clear();
 
@@ -15,28 +16,7 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("listener-service");
-
-  const subscription = stan.subscribe(
-    "ticket:create",
-    "queue-group-name",
-    options
-  );
-
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === "string") {
-      console.log(
-        `Recieved event number ${msg.getSequence()}, with data: ${data}`
-      );
-    }
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -6,16 +7,18 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: 12,
-    title: "concert",
-    price: 23,
-  });
+  const publisher = new TicketCreatedPublisher(stan);
 
-  stan.publish("ticket:create", data, () => {
-    console.log("Event publish");
-  });
+  try {
+    await publisher.publish({
+      id: "12",
+      title: "concert",
+      price: 23,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });

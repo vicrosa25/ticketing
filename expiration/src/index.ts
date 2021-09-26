4d709a4db7d4238641d5b39a047d98fe5e7cda21
victor rosa <vicrosa25@gmail.com>
2021-09-26 18:55:25 +0200

    Expiration Service Setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,36 @@
+import { natsWrapper } from "./nat-wrapper";
+
+// Start the Server and the Mongo Database
+const start = async () => {
+  console.log("starting...");
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be define");
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be define");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be define");
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
